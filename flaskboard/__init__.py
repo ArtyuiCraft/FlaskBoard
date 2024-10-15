e91064cc1d5fc03b187ba0573be9120ae3926220
ArtyuiCraft <artyuicraft@gmail.com>
2024-10-15 22:28:35 +0200

    changed instance folder

--- flaskboard/__init__.py
+++ flaskboard/__init__.py
@@ -1,8 +1,11 @@
 import os
 from flask import Flask
 
+def start(*args,**kwargs):
+    create_app()
+
 def create_app(test_config=None):
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__,instance_path='/forum/instance', instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskboard.sqlite'),
