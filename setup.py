de5bf8237d51bd531cf8b99a01fb2667de55ede1
ArtyuiCraft <artyuicraft@gmail.com>
2024-10-15 22:31:34 +0200

    changed instance folder

--- setup.py
+++ setup.py
@@ -2,3 +2,5 @@ import os
 
 if not os.path.exists("./instance") or not os.path.exists("./instance/flaskboard.sqlite"):
     os.system("flask --app flaskboard init-db")
+if not os.path.exists("/forum/instance") or not os.path.exists("/forum/instance/flaskboard.sqlite"):
+    os.system("flask --app flaskboard init-db")
