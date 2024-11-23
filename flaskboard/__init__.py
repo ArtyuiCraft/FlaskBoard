34ea22eed2230f4c0889198ab7c546f88ba7b9f6
Artyuicraft <Artyuicraft@gmail.com>
2024-11-23 10:42:56 +0100

    Vercel test

--- flaskboard/__init__.py
+++ flaskboard/__init__.py
@@ -18,10 +18,6 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
-
     from . import blog
     from . import db
     from . import auth
